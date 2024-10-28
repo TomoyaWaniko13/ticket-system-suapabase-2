4d35a2e374d2f601877e951bb4765d6c999fc94f
tomoya <alligatorfree12@gmail.com>
2024-10-28 13:07:00 +0900

    P.42 Utilizing createBrowserClient on the frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 
-import { createSupabaseClient } from '@/supabase-utils/browserClient';
 import { useEffect } from 'react';
+import { getSupabaseBrowserClient } from '@/supabase-utils/browserClient';
 
 // P.38 Initializing and testing the base Supabase JavaScript client within Next.js
+// P.42 Utilizing createBrowserClient on the frontend
 const HomePage = () => {
   useEffect(() => {
-    const supabase = createSupabaseClient();
+    const supabase = getSupabaseBrowserClient();
     supabase.storage.listBuckets().then((result) => {
       console.log('Bucket List', result);
     });
