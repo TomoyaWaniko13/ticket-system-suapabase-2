e8fdb99016edd6969873625da581f8d7431f77e7
tomoya <alligatorfree12@gmail.com>
2024-10-30 05:27:17 +0900

    P.86 Enhancing the navigation component

--- app/tickets/Nav.tsx
+++ app/tickets/Nav.tsx
@@ -1,34 +1,34 @@
+'use client';
+
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { usePathname } from 'next/navigation';
 
 // P.69 Creating a shared UI layout with navigation elements
 // P.75 Constructing the Ticket Details page
+// P.86 Enhancing the navigation component
+
+const links = [
+  { label: 'Ticket List', href: '/tickets' },
+  { label: 'Create new Ticket', href: '/tickets/new' },
+  { label: 'Ticket List', href: '/tickets/users' },
+];
 
 export default function Nav() {
+  const pathname = usePathname();
+
   return (
     <nav className={'flex justify-between'}>
       <ul className={'flex space-x-3'}>
-        <li>
-          <Button asChild>
-            <Link role='button' href='/tickets'>
-              Ticket List
-            </Link>
-          </Button>
-        </li>
-        <li>
-          <Button asChild>
-            <Link role='button' href='/tickets/new'>
-              Create new Ticket
-            </Link>
-          </Button>
-        </li>
-        <li>
-          <Button asChild>
-            <Link role='button' href='/tickets/users'>
-              User List
-            </Link>
-          </Button>
-        </li>
+        {links.map((link) => (
+          <li>
+            <Button asChild variant={pathname === link.href ? 'default' : 'outline'}>
+              <Link role={'button'} href={link.href}>
+                {link.label}
+              </Link>
+            </Button>
+          </li>
+        ))}
       </ul>
       <ul>
         <Button asChild variant={'secondary'}>
